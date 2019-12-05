5c5491af7f49d45c7fe8ec674ccc25fd17fe1115
Tim Creasy <timcreasy@me.com>
2019-12-04 22:18:32 -0600

    Day four, part two

--- src/day_four.clj
+++ src/day_four.clj
@@ -14,8 +14,26 @@
          (apply <= (map #(Integer/parseInt %)
                         (map str s))))))
 
+(defn valid-password?' [pw]
+  (let [s (str pw)
+        digits (map str s)]
+    (and (= (count s) 6)
+         (some (partial apply =)
+               (partition 2 1 s))
+         (apply <= (map #(Integer/parseInt %)
+                        digits))
+         (some (fn [[k v]]
+                 (= v 2))
+               (frequencies digits)))))
+
 (defn part-one [input]
   (count (filter valid-password?
                  (puzzle-input->range input))))
 
-(part-one "124075-580769")
+(defn part-two [input]
+  (count (filter valid-password?'
+                   (puzzle-input->range input))))
+
+;(part-one "124075-580769")
+
+;(part-two "124075-580769")
